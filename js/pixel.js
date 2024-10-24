eeae8a7c280f26d6121d5c1b168293e207812bd7
DavidPro1997 <david_pro1997@outlook.com>
2024-10-24 17:13:46 -0500

    cd

--- js/pixel.js
+++ js/pixel.js
@@ -1,43 +1,37 @@
 var pixelId = '8569429083102717';
 
 function cargarPixel() {
-    const pixelPuntana = `
-        <!-- Meta Pixel Code -->
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${pixelId}');
-        fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1"
-        /></noscript>
-        <!-- End Meta Pixel Code -->
-    
+    // Verifica si el pixel ya está cargado
+    if (!window.fbq) {
+        // Crear el script para cargar el pixel
+        const scriptPixel = document.createElement('script');
+        scriptPixel.async = true;
+        scriptPixel.src = "https://connect.facebook.net/en_US/fbevents.js";
+
+        // Inicializar el pixel
+        const pixelInit = document.createElement('script');
+        pixelInit.innerHTML = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
         `;
 
-    // Inyectar el contenido de pixelPuntana en el DOM
-    const div = document.createElement('div');
-    div.innerHTML = pixelPuntana;
-    document.head.appendChild(div);
+        // Agregar scripts al <head>
+        document.head.appendChild(scriptPixel);
+        document.head.appendChild(pixelInit);
 
-    // Ejecutar el código dentro del script manualmente
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-    document.head.appendChild(script);
-
-    window.fbq = window.fbq || function() {
-        (window.fbq.q = window.fbq.q || []).push(arguments);
-    };
-    fbq('init', pixelId);
-    fbq('track', 'PageView');
+        // Inyectar el <noscript> para navegadores sin JS
+        const noscript = document.createElement('noscript');
+        noscript.innerHTML = `<img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1" />`;
+        document.body.appendChild(noscript);
+    }
 }
 
